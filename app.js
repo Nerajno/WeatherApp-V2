5f8348dc969298d0cb07adfa15504734f109d2d1
Nerajno <Nerajno@gmail.com>
2017-08-17 12:33:51 -0400

    trying

--- app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function){
-
-  var lat;
-  var long;
-  
-
-}
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,32 @@
-$(document).ready(function){
+$(document).ready(function(){
 
   var lat;
   var long;
-  
 
-}
+// Documentation from Mdn geolocation for html5
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+function success(pos) {
+  var crd = pos.coords;
+  console.log('Your current position is:');
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+  // console.log(crd.latitude + "1");
+  lat = crd.latitude;
+  
+};
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+};
+navigator.geolocation.getCurrentPosition(success, error, options);
+});
+
+//googleapis
+// var apiKey = "AIzaSyAkPcGdzzwW6UzzC2HmOJCf1_HSedXNCvo"
+// var api = "http://ip-api.com/json/?callback=yourfunction";
+// var red = "blue";
