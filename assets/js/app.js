7433d14fcd0ea87f591636cfa259e0bee19ef8fb
Nerajno <Nerajno@gmail.com>
2017-08-30 12:47:09 -0400

    still wrking on it

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,61 +4,82 @@ $(document).ready(function(){
   var location;
   var state;
   var city;
-  // Documentation from Mdn geolocation for html5
+
+  // Documentation from Mdn geolocation for html5 and its accuracy
   var options = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0
   };
-  function success(pos) {
+
+  function success(pos){
     var crd = pos.coords;
-    /*console.log('Your current position is:');
+
+    /* => Previous Code
+    console.log('Your current position is:');
     console.log(`Latitude : ${crd.latitude}`);
     console.log(`Longitude: ${crd.longitude}`);
     console.log(`More or less ${crd.accuracy} meters.`);
     console.log(crd.latitude + "1");*/
+
     lat = crd.latitude.toFixed(2);
     long= crd.longitude.toFixed(2);
-    // console.log(lat,long);
-    // testing weather function
+
     var weather = function(long,lat){
       var apiKey ="eefb3de557ed0c0a";
       var api = "http://api.wunderground.com/api/"+apiKey +"/geolookup/q/" +long+ "," + lat +".json";
-      // var state = "";
+      
       console.log(api);
 
       // JSON Call to get location
       $.getJSON(api,function(data){
-        location = data.location.city.replace(/ /g, "_");
-        state = data.location.state;
+        location = data.location.city.replace(/ /g,"_");
+        state = data.location.state
+        $('#location').html(location+","+state);
         console.log(location,state);
 
         var api2 = "http://api.wunderground.com/api/"+apiKey +"/conditions/q/"+state+"/"+location+".json";
+        console.log(api2);
         $.getJSON(api2,function(data){
           //console...... other stuff
-          var marco = data.current_observation.temperature_string;
-          console.log(marco);
 
+          var weatherInfo = data.current_observation.weather;
+          $('#weatherInfo').html(weatherInfo)
+          console.log(weatherInfo);
+          // var marco = data.current_observation.temperature_string;
+          // console.log(marco);
+
+
+          //DATE AND TIME//
+          //Converted into days, months, hours, day-name, AM/PM
+          //Understood it but it took too long to code.
+          var dt = new Date();
+          var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+          $('#day').html(days[dt.getDay()]);
+          var months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+          $('#date').html(months[dt.getMonth()] + " " + dt.getDate() + ", " + dt.getFullYear());
+          $('#time').html((dt.getHours()>12?(dt.getHours()-12):dt.getHours()).toString() + ":" + ((dt.getMinutes() < 10 ? '0' : '').toString() + dt.getMinutes().toString()) + (dt.getHours() < 12 ? ' AM' : ' PM').toString());
         });
-        console.log(api2);
       });
-
-
-
-
     };
-    weather(lat,long,state,city)
-
-  };
+    weather(lat,long);
+    };
+  
   function error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
+  console.warn(`ERROR(${err.code}): ${err.message}`);
   };
+
   navigator.geolocation.getCurrentPosition(success, error, options);
+
 });
 
 
 
-//googleapis
-// var apiKey = "AIzaSyAkPcGdzzwW6UzzC2HmOJCf1_HSedXNCvo"
-// var api = "http://ip-api.com/json/?callback=yourfunction";
-// var red = "blue";
+
+
+
+
+/*Testing Information
+googleapis
+var apiKey = "AIzaSyAkPcGdzzwW6UzzC2HmOJCf1_HSedXNCvo"
+var api = "http://ip-api.com/json/?callback=yourfunction"*/
