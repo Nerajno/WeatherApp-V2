3611d9451565db730f2bdeced1e9083bdfa64cb0
Nerajno <Nerajno@gmail.com>
2017-08-30 20:14:17 -0400

    added humidity and pressure to the index

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,18 +21,18 @@ $(document).ready(function(){
     console.log(`Longitude: ${crd.longitude}`);
     console.log(`More or less ${crd.accuracy} meters.`);
     console.log(crd.latitude + "1");*/
-   
-       
 
 
-    // Rounding down the deciamal place of the location 
+
+
+    // Rounding down the deciamal place of the location
     lat = crd.latitude.toFixed(2);
     long= crd.longitude.toFixed(2);
 
     var weather = function(long,lat){
       var apiKey ="eefb3de557ed0c0a";
       var api = "http://api.wunderground.com/api/"+apiKey +"/geolookup/q/" +long+ "," + lat +".json";
-      
+
       // console.log(api);
 
       // JSON Call to get location
@@ -56,20 +56,22 @@ $(document).ready(function(){
           $("#weather_icon").attr("src",weather_icon);
           // console.log(weather_icon);
 
-         // // CHAnge me 
+         // // CHAnge me
          // https://sunrise-sunset.org/api
           var api3 = "https://api.sunrise-sunset.org/json?lat=" +lat+'&lon=' +long;
           // "https://fcc-weather-api.glitch.me/api/current?lat=" +lat+'&lon=' +long;
           console.log(api3);
-  
+
           var windSpeed = data.current_observation.wind_mph;
           $("#windSpeed").html(windSpeed+" mph");
           // console.log(windSpeed);
 
           var humidity = data.current_observation.relative_humidity;
+          $("#humidity").html(humidity+ " %");
           // console.log(humidity);
 
           var pressure = data.current_observation.pressure_mb;
+          $("#pressure").html(pressure+ " %");
           // console.log(pressure);
 
           // var marco = data.current_observation.temperature_string;
@@ -91,7 +93,7 @@ $(document).ready(function(){
     };
     weather(lat,long);
     };
-  
+
   function error(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
   };
